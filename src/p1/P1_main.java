360373ee3e8aab164dcd322c05bd58bf56cc973e
Pizzarunnerand7 <chrisalme@gmx.net>
2025-11-03 12:24:05 +0100

    6.commit

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -3,7 +3,16 @@ package p1;
 public class P1_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int wuerfel = 2;
+		if (wuerfel == 1 || wuerfel == 4) {
+			System.out.println("Gewonnen!");
+		}
+		if (wuerfel == 2) {
+			System.out.println("verloren");
+		}
+		if (wuerfel == 5) {
+			System.out.println("Unentschieden");
+		}
 
 	}
 
