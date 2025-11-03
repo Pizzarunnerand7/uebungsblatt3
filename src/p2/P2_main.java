d822138d345a387c570d9f0010378b6eb9ea0bb9
Pizzarunnerand7 <chrisalme@gmx.net>
2025-11-03 12:32:04 +0100

    7.commit

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -3,7 +3,28 @@ package p2;
 public class P2_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 100;
+		int b = 4;
+		int c = 1;
+		int d = 2;
+		d=0;
+		boolean E1 = a == b;
+		boolean E2 = a == b || a == c;
+		boolean E3 = a+b >= c;
+		boolean E4 = a+b+c >= 100;
+		if (E1) {
+			d++;
+		}
+		if (E2) {
+			d++;
+		}
+		if (E3) {
+			d++;
+		}
+		if (E4) {
+			d++;
+		}
+		System.out.println(d);
 
 	}
 
